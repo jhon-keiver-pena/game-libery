27b97a62de91d8d4f4678f13ed7187ce206e5eec
Jhonconnors <anonimat.jk@gmail.com>
2024-10-08 20:38:59 -0300

    se agrega la parte visual de reserva y se termina lÃa biblioteca y el home

--- app/src/main/java/com/example/library/Home.java
+++ app/src/main/java/com/example/library/Home.java
@@ -3,7 +3,6 @@ package com.example.library;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +11,7 @@ import com.example.gamelibery.R;
 
 public class Home extends AppCompatActivity {
 
-    private Button btnCrear;
+    private Button btnCrear, btnReserva;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +19,20 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         btnCrear = findViewById(R.id.ir_biblioteca);
+        btnReserva = findViewById(R.id.btn_reservas);
 
         // Acción del botón
         btnCrear.setOnClickListener(view -> {
             //redirige a un activity
             Intent intent = new Intent(getBaseContext(), BibliotecaMaestro.class);
+            startActivity(intent);
+            finish();
+        });
+
+        // Acción del botón
+        btnReserva.setOnClickListener(view -> {
+            //redirige a un activity
+            Intent intent = new Intent(getBaseContext(), ListaReservaActivity.class);
             startActivity(intent);
             finish();
         });
